cd1e2dde933391fda52a82393ef5a7b0c9b6821f
Brad Micklea <7644938+bmicklea@users.noreply.github.com>
2024-10-22 13:51:20 -0400

    pull-to-unpack

--- modelkit_helpers.py
+++ modelkit_helpers.py
@@ -6,8 +6,8 @@ def kit_login(user: str, passwd: str, registry: str = "jozu.ml"):
 def kit_logout(registry: str = "jozu.ml"):
     subprocess.run(["kit", "logout", registry])
 
-def kit_pull(repo_path_with_tag):
-    subprocess.run(["kit", "pull", repo_path_with_tag])
+def kit_unpack(repo_path_with_tag):
+    subprocess.run(["kit", "unpack", repo_path_with_tag])
 
 def kit_pack(repo_path_with_tag: str):
     subprocess.run(["kit", "pack", ".", "-t", repo_path_with_tag])
@@ -15,11 +15,11 @@ def kit_pack(repo_path_with_tag: str):
 def kit_push(repo_path_with_tag):
     subprocess.run(["kit", "push", repo_path_with_tag])
 
-def pull_collated_data_modelkit(user: str, passwd: str, namespace: str = "jozu-demos",
+def unpack_collated_data_modelkit(user: str, passwd: str, namespace: str = "jozu-demos",
                                 registry: str = "jozu.ml", tag: str = "collated-data-v1"):
     repo_path_with_tag = registry + "/" + namespace + "/" + "titanic-survivability" + ":" + tag
     kit_login(user, passwd, registry)
-    kit_pull(repo_path_with_tag)
+    kit_unpack(repo_path_with_tag)
     kit_logout()
 
 def pack_and_push_modelkit(user: str, passwd: str, namespace: str, 
