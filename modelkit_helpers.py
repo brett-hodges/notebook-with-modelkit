45c71d17019f9437eb79c1af39e64abf5bd2828d
Brett Hodges <brett@jozu.com>
2024-10-22 13:20:24 -0500

    add command echoing

--- modelkit_helpers.py
+++ modelkit_helpers.py
@@ -1,19 +1,29 @@
 import subprocess
 
 def kit_login(user: str, passwd: str, registry: str = "jozu.ml"):
-    subprocess.run(["kit", "login", registry, "-u", user, "--password-stdin"], input=passwd, text=True)
+    command = ["kit", "login", registry, "-u", user, "--password-stdin"]
+    print(' '.join(command))
+    subprocess.run(command, input=passwd, text=True)
 
 def kit_logout(registry: str = "jozu.ml"):
-    subprocess.run(["kit", "logout", registry])
+    command = ["kit", "logout", registry]
+    print(' '.join(command))
+    subprocess.run(command)
 
 def kit_unpack(repo_path_with_tag):
-    subprocess.run(["kit", "unpack", "-o", repo_path_with_tag])
+    command = ["kit", "unpack", "-o", repo_path_with_tag]
+    print(' '.join(command))
+    subprocess.run(command)
 
 def kit_pack(repo_path_with_tag: str):
-    subprocess.run(["kit", "pack", ".", "-t", repo_path_with_tag])
+    command = ["kit", "pack", ".", "-t", repo_path_with_tag]
+    print(' '.join(command))
+    subprocess.run(command)
 
 def kit_push(repo_path_with_tag):
-    subprocess.run(["kit", "push", repo_path_with_tag])
+    command = ["kit", "push", repo_path_with_tag]
+    print(' '.join(command))
+    subprocess.run(command)
 
 def unpack_collated_data_modelkit(user: str, passwd: str, namespace: str = "jozu-demos",
                                 registry: str = "jozu.ml", tag: str = "collated-data-v1"):
